bcac02de86bd785c35bbd2223da4a738e2e08275
nick2474 <nick2474@hotmail.com>
2020-04-05 23:07:01 -0600

    API Listing project with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-dream-app';
+
+export class AppComponent implements OnInit {
+  users$: User[];
+  constructor(private Dataservice: DataService){}
+  ngOnInit(){
+    return this.Dataservice.getUsers()
+    .subscribe(data => this.users$ = data);
+  }
 }
